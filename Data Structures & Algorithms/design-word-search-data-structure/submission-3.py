c75cf7ae9b8620322cf670ed35b3b431075f81b3
adkushal <adkushal@gmail.com>
2026-08-27 18:33:11 -0700

    Add: design-word-search-data-structure - submission-3

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-3.py
@@ -0,0 +1,52 @@
+class TreeNode:
+    def __init__(self):
+        self.children = {}
+        self.isWord = False
+
+class WordDictionary:
+
+    def __init__(self):
+        self.root = TreeNode()
+        
+
+    def addWord(self, word: str) -> None:
+        curr = self.root
+        for ch in word:
+            if ch not in curr.children:
+                curr.children[ch] = TreeNode()
+            curr = curr.children[ch]
+        curr.isWord = True
+        
+
+    def search(self, word: str) -> bool:
+        def dfs(node, word):
+            # Check the end cases
+            # No children left
+            if not node:
+                return False
+
+            if len(word) == 0:
+                return node.isWord
+
+            # chars left, search in children
+            # First char of word
+            ch = word[0]
+            # is a "."
+            if ch == ".":
+                result = False
+                for child in node.children.values():
+                    result = result or dfs(child, word[1:])
+                return result
+
+            else:
+                if ch not in node.children:
+                    return False
+                return dfs(node.children[ch], word[1:])
+
+        return dfs(self.root, word)
+            
+
+
+
+            
+        
